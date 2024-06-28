f695c0a9347b8113a39539b0cbbffb8e4a6b1f4b
ChezyName <mineboy2468@gmail.com>
2024-06-27 19:48:29 -0500

    1 Hour Finished.

---
+++ main.js
@@ -0,0 +1,47 @@
+//While the page is loaded, keep clicking on check boxes
+var inputs = document.querySelectorAll("input[type='checkbox']");
+
+function doSome(currentIndex){
+    //get random number between 50 and 200
+    //clamp from currentIndex - max input
+    let amount = currentIndex + Math.floor(Math.random() * 3) + 1;
+    amount = currentIndex + 1;
+    amount = Math.min(Math.max(amount, currentIndex), inputs.length);
+
+    for(let i = currentIndex; i < amount; i++){
+        if(!inputs[i].checked){
+            inputs[i].click();
+            console.log("Auto Clicking: " + inputs[i].id);
+        }
+    }
+
+    return amount;
+}
+
+let index = 0;
+function main() {
+    inputs = document.querySelectorAll("input[type='checkbox']");
+    //console.log(inputs);
+
+    if(inputs.length == 0 || inputs.length == undefined || inputs.length == NaN || inputs.length == null){
+        //Stil loading checkboxes
+        console.log("inputs array length == 0")
+        let randomWaitMS = Math.floor(Math.random() * 1500) + 300;
+        setTimeout(main, randomWaitMS);
+    }
+    else {
+        console.log("Running Click Starting From: " + index);
+        index = doSome(index);
+        console.log("Finished Click Ending At: " + index);
+    
+        if(index > inputs.length) index = 0;
+    
+        //wait some time
+        let randomWaitMS = 125;
+        setTimeout(main, randomWaitMS);
+    }
+}
+
+//Wait for page to load
+console.log("Loaded Auto Checkbox Clicker - By ChezyName")
+main();
